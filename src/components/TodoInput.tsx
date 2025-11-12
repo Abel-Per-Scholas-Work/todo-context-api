88b995bcdaee31f30048e61628853c745c8bc1c3
belunatic <abelluvyou@yahoo.com>
2025-11-12 12:04:56 -0800

    feat: add task to the todo list

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,11 +1,30 @@
+import { useContext, useState } from "react";
+import { TodoContext } from "../context/TodoContext";
+
 export default function TodoInput() {
+	const { addTodo } = useContext(TodoContext);
+	const [text, setText] = useState("");
+	const [errortext, setErrorText] = useState(false);
+
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+		if (!text) {
+			setErrorText(true);
+			return;
+		}
+		addTodo(text);
 	};
 	return (
 		<div>
-			<form>
-				<input type="text" placeholder="What needs to be done" />
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="What needs to be done"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+					required
+				/>
+				{errortext && <p>Invalid Input</p>}
 				<button>Add todo</button>
 			</form>
 		</div>
