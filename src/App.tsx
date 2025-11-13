ff43a66cb2cc12b3773ceefc77b2da57b92c7a29
belunatic <abelluvyou@yahoo.com>
2025-11-12 21:17:58 -0800

    add dark and light theme css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import "./App.css";
 import FilterButton from "./components/FilterButton";
 import ThemeToggleButton from "./components/ThemeToggleButton";
 import TodoInput from "./components/TodoInput";
 import ToDosList from "./components/TodosList";
+import { ThemeContext } from "./context/ThemeContext";
 
 function App() {
+	const { theme } = useContext(ThemeContext);
 	return (
 		<>
-			<main>
+			<main
+				className={`max-w-[85rem] mx-auto ${
+					theme === "light" ? "light" : "dark"
+				}`}>
 				<header>
 					<div>Todo App (Context API)</div>
 					<div>
