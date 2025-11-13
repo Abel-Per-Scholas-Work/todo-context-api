53e08e4e7a84a5d5182c3b8d10b7041a3bb3e953
belunatic <abelluvyou@yahoo.com>
2025-11-12 17:49:47 -0800

    feat: show a message when no todos in the list

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -3,7 +3,7 @@ import { TodoContext } from "../context/TodoContext";
 import { FilterContext } from "../context/FilterContext";
 
 export default function ToDosList() {
-	const { todos } = useContext(TodoContext);
+	const { todos, deleteTodo, toggleTodo } = useContext(TodoContext);
 	const { filter } = useContext(FilterContext);
 
 	const filteredTodos = todos.filter((todo) => {
@@ -16,23 +16,27 @@ export default function ToDosList() {
 		<div>
 			<h2>Todos List</h2>
 			<div>Filter: {filter}</div>
-			{filteredTodos.map((todo) => (
-				<div key={todo.id}>
-					<p>
-						<input
-							type="checkbox"
-							name={todo.text}
-							checked={todo.completed}
-							onChange={() => console.log("clicked")}
-						/>
-						{todo.text}
-						<div>
-							<button onClick={() => console.log("edit")}>✏️</button>
-							<button onClick={() => console.log("delete")}>🗑️</button>
-						</div>
-					</p>
-				</div>
-			))}
+			{filteredTodos.length === 0 ? (
+				<p> No todos yet! Add one above.</p>
+			) : (
+				filteredTodos.map((todo) => (
+					<div key={todo.id}>
+						<p>
+							<input
+								type="checkbox"
+								name={todo.text}
+								checked={todo.completed}
+								onChange={() => toggleTodo(todo.id)}
+							/>
+							{todo.text}
+							<div>
+								<button onClick={() => console.log("edit")}>✏️</button>
+								<button onClick={() => deleteTodo(todo.id)}>🗑️</button>
+							</div>
+						</p>
+					</div>
+				))
+			)}
 		</div>
 	);
 }
